e56e5d91f071a4436635dabcc8da25953aaa8bf7
Renanppcosta <renanppcosta@gmail.com>
2024-11-05 23:55:39 -0300

    feat: model events and process of create event created

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    STATUS_CHOICES = [
+    ("Planejando", "Planejando"),
+    ("Ativo", "Ativo"),
+    ("Cancelado", "Cancelado"),
+    ]
+
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    status = models.CharField(max_length=100)
+    date = models.DateField()
+    location = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="Planejando")
+
+    def __str__(self):
+        return self.title
+
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,30 @@
+from datetime import datetime
 from django.shortcuts import render
+from .models import Events
 
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    if request.method == "GET":
+        return render(request, "home.html")
+    elif request.method == "POST":
+        title = request.POST.get("title-role")
+        description = request.POST.get("description")
+        date = request.POST.get("date-role")
+        location = request.POST.get("location")
+
+        event_date = datetime.fromisoformat(date)
+
+        if event_date < datetime.now():
+            return render(request, "home.html")
+        
+        event = Events(
+            title=title,
+            description=description,
+            date=event_date,
+            location=location
+        )
+        
+        event.save()
+
+        return render(request, "home.html")
