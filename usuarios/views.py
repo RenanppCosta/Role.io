d3563b6d4f5fc04a0b0abd5aec098efc0033e728
Renanppcosta <renanppcosta@gmail.com>
2024-11-06 16:20:32 -0300

    feat: get method rendering database items and correcting date logic

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,23 +1,29 @@
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Events
 
 # Create your views here.
 
 def home(request):
     if request.method == "GET":
-        return render(request, "home.html")
+        events = Events.objects.all()
+
+        return render(request, "home.html", {"events": events})
+    
     elif request.method == "POST":
         title = request.POST.get("title-role")
         description = request.POST.get("description")
         date = request.POST.get("date-role")
         location = request.POST.get("location")
-
-        event_date = datetime.fromisoformat(date)
-
-        if event_date < datetime.now():
+        
+        if date:
+            event_date = datetime.fromisoformat(date)
+            if event_date < datetime.now():
+                return render(request, "home.html")
+        else:
             return render(request, "home.html")
         
+            
         event = Events(
             title=title,
             description=description,
@@ -27,4 +33,4 @@ def home(request):
         
         event.save()
 
-        return render(request, "home.html")
+        return redirect("home")
